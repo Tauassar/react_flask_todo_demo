954ada47d633e2fc917e79329ffa507623dc5936
tauassar <tauassar.tatiyev@nu.edu.kz>
2022-07-16 17:23:51 +0600

    Change default env from prod to test

--- flask_backend/config.py
+++ flask_backend/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class BaseConfig:
-    DEBUG = False
+    DEBUG = True
     SECRET_KEY = '\xb2\xae\x00\x87\x00\xde\x16L\xa1PD\\\xe7\xcf\x8b\x11'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
